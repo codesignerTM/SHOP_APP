74582e6dcc46ec873858a5dc5ae7ca146d2c7164
Tamás <codesigner.hu@gmail.com>
2020-01-04 14:08:25 +0100

    adding product data to screen from redux

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,7 +1,17 @@
 import React from "react";
 import { View, Text, FlatList, StyleSheet } from "react-native";
+import { userSelector, useSelector } from "react-redux";
 
-const ProductsOverViewScreen = props => {};
+const ProductsOverViewScreen = props => {
+  const products = useSelector(state => state.products.availableProducts);
+  return (
+    <FlatList
+      data={products}
+      keyExtractor={item => item.id}
+      render={itemData => <Text>{itemData.item.title}</Text>}
+    />
+  );
+};
 
 const styles = StyleSheet.create({});
 
