d15e3f07fbf015d17b93ea51326c606c146fec28
Tamás <codesigner.hu@gmail.com>
2020-01-04 20:48:46 +0100

    rendering product titles

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,16 +1,25 @@
 import React from "react";
 import { View, Text, FlatList, StyleSheet } from "react-native";
-import { userSelector, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 
 const ProductsOverViewScreen = props => {
   const products = useSelector(state => state.products.availableProducts);
+  console.log("products", products);
   return (
     <FlatList
       data={products}
       keyExtractor={item => item.id}
-      render={itemData => <Text>{itemData.item.title}</Text>}
+      renderItem={itemData => (
+        <Text style={{ color: "black" }}>{itemData.item.title}</Text>
+      )}
     />
   );
+};
+
+ProductsOverViewScreen.navigationOptions = () => {
+  return {
+    headerTitle: "All Products"
+  };
 };
 
 const styles = StyleSheet.create({});
