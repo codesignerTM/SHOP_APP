7aa4e1417d9006a210a7bee9dc9e952b0db3ab5c
Tamás <codesigner.hu@gmail.com>
2020-01-05 10:23:35 +0100

    create reducer & action for adding to cart

---
+++ store/reducers/cart.js
@@ -0,0 +1,37 @@
+import PRODUCTS from "../../data/dummy-data";
+import { ADD_TO_CART } from "../actions/cart";
+import CartItem from "../../models/cartItem.model";
+
+const initialState = {
+  items: {},
+  totalAMount: 0
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TO_CART:
+      const addedProduct = action.product;
+      const prodPrice = addedProduct.price;
+      const prodTitle = addedProduct.title;
+
+      let updatedOrNewProduct;
+
+      if (state.items[addedProduct.id]) {
+        //already have the item in the cart
+        updatedOrNewProduct = new CartItem(
+          state.items[addedProduct.id].quantity + 1,
+          prodPrice,
+          prodTitle,
+          state.items[addedProduct.id].sum + prodPrice
+        );
+      } else {
+        updatedOrNewProduct = new CartItem(1, prodPrice, prodTitle, prodPrice);
+      }
+      return {
+        ...state,
+        items: { ...state.items, [addedProduct.id]: updatedOrNewProduct },
+        totalAMount: state.totalAMount + prodPrice
+      };
+  }
+  return state;
+};
