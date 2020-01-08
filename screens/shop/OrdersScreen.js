72fbfc5c32e482158f8544f7f5c50a0440bdde9b
Tamás <codesigner.hu@gmail.com>
2020-01-08 21:59:12 +0100

    creating orders screen

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,7 +1,37 @@
 import React from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import { Text, FlatList, StyleSheet, Platform } from "react-native";
+import { useSelector } from "react-redux";
+import CustomHeaderButton from "../../components/UI/HeaderButton";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
-const OrdersScreen = props => {};
+const OrdersScreen = props => {
+  const orders = useSelector(state => state.orders.orders);
+
+  return (
+    <FlatList
+      data={orders}
+      keyExtractor={item => item.id}
+      renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>}
+    />
+  );
+};
+
+OrdersScreen.navigationOptions = navData => {
+  return {
+    headerTitle: "Your Orders",
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          iconName="menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+          title="Shopping cart"
+        ></Item>
+      </HeaderButtons>
+    )
+  };
+};
 
 const styles = StyleSheet.create({});
 
