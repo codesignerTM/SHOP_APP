e48ca7529ccbd8767380d6f32216823272461eab
Tamás <codesigner.hu@gmail.com>
2020-01-09 19:13:37 +0100

    Creating user products screen

--- screens/user/UserProductsScreen.js
+++ screens/user/UserProductsScreen.js
@@ -1,7 +1,47 @@
 import React from "react";
-import { View, Text, FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, Platform } from "react-native";
+import { useSelector } from "react-redux";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
-const UserProductsScreen = props => {};
+import ProductItem from "../../components/shop/ProductItem";
+import CustomHeaderButton from "../../components/UI/HeaderButton";
+
+const UserProductsScreen = props => {
+  const userProducts = useSelector(state => state.products.userProducts);
+  console.log("userProducts", userProducts);
+  return (
+    <FlatList
+      data={userProducts}
+      keyExtractor={item => item.id}
+      renderItem={itemData => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+        />
+      )}
+    />
+  );
+};
+
+UserProductsScreen.navigationOptions = navData => {
+  return {
+    headerTitle: "Your Products",
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          iconName="menu"
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+          title="Shopping cart"
+        ></Item>
+      </HeaderButtons>
+    )
+  };
+};
 
 const styles = StyleSheet.create({});
 
